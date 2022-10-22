d8c9f08dc23ddd72f6a08269c180f3836ef97bdf
Rick Kersmarki <rkersmarki@gmail.com>
2022-10-22 09:23:19 -0500

    base repository

---
+++ Granny's-Hot-Box/Repositories/BaseRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace Granny_s_Hot_Box.Repositories
+{
+    public class BaseRepository
+    {
+        private readonly string _connectionString;
+        protected BaseRepository(IConfiguration config)
+        {
+            _connectionString = config.GetConnectionString("DefaultConnection");
+        }
+
+        public SqlConnection Connection => new SqlConnection(_connectionString);
+    }
+}
