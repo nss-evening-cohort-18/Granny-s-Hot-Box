841d3b377d52f880524eb9ad1aa3ab26792a8af5
Nathan Blackman <nathanpgblackman@gmail.com>
2022-10-22 10:41:50 -0500

    Safety Commit

---
+++ Granny's-Hot-Box/Interfaces/UserInterface.cs
@@ -0,0 +1,13 @@
+﻿using Granny_s_Hot_Box.Models;
+
+namespace Granny_s_Hot_Box.Interfaces
+{
+    public interface UserInterface
+    {
+        public List<Users> GetAllUsers();
+        public void AddUser(Users user);
+        public void UpdateUser(Users user);
+        public void DeleteUser(string FirebaseId);
+        public Users GetByFirebaseId(string FirebaseId);
+    }
+}
