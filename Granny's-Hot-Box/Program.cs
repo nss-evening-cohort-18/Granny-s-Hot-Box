6246079562b7ad65c998f46d70384d77dc163b11
GiovanniGith <Gbarajas2109@gmail.com>
2022-10-29 13:38:41 -0500

    New PaymentType with updates to Model Controller Interface Repository

--- Granny's-Hot-Box/Program.cs
+++ Granny's-Hot-Box/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddTransient<IUser, UserRepository>();
 builder.Services.AddTransient<IMealProduct, MealProductRepository>();
+builder.Services.AddTransient<IPaymentType, PaymentTypeRepository>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
---
+++ Granny's-Hot-Box/Repositories/PaymentTypeRepository.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Data.SqlClient;
+using Granny_s_Hot_Box.Models;
+using Granny_s_Hot_Box.Interfaces;
+
+
+namespace Granny_s_Hot_Box.Repositories
+{
+    public class PaymentTypeRepository : BaseRepository, IPaymentType
+    {
+        private readonly string _baseSqlSelect = @"SELECT Id,
+                                                    Type
+                                                   FROM PaymentType";
+
+        public PaymentTypeRepository(IConfiguration config) : base(config) { }
+
+        public List<PaymentType> GetAllPaymentTypes()
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = _baseSqlSelect;
+
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        var results = new List<PaymentType>();
+                        while (reader.Read())
+                        {
+                            var pt = LoadFromData(reader);
+
+                            results.Add(pt);
+                        }
+
+                        return results;
+                    }
+                }
+            }
+        }
+
+      
+
+      private PaymentType LoadFromData(SqlDataReader reader)
+        {
+            return new PaymentType
+            {
+                Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                Type = reader.GetString(reader.GetOrdinal("Type")),
+               
+            };
+        }
+    }
+}
