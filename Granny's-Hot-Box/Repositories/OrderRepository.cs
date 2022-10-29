43bc83080e9ee9fff0f228e99bec483f700cbe24
Nathan Blackman <nathanpgblackman@gmail.com>
2022-10-29 13:44:32 -0500

    Pushing up Orders

--- Granny's-Hot-Box/Repositories/OrderRepository.cs
+++ Granny's-Hot-Box/Repositories/OrderRepository.cs
@@ -4,12 +4,14 @@ namespace Granny_s_Hot_Box.Repositories
 {
     public class OrderRepository
     {
-        private readonly IConfiguration _config; 
-        
+        private readonly IConfiguration _config;
+
         public OrderRepository(IConfiguration config)
         {
             _config = config;
         }
+
+/*      NATHAN WORKING
         public SqlConnection Connection
         {
             get
@@ -17,5 +19,15 @@ namespace Granny_s_Hot_Box.Repositories
                 return new SqlConnection
             }
         }
+
+        public List<Orders> GetAllOrders
+        {
+            using (SqlConnection conn = Connection)
+            {
+                cmd.CommandText = @"
+            "
+            }
+        }
+*/
     }
 }
