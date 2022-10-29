96a5c3550e7992a5cde915123e9fc1ce2224cd1a
Nathan Blackman <nathanpgblackman@gmail.com>
2022-10-29 11:07:34 -0500

    Safety Commit

---
+++ Granny's-Hot-Box/Repositories/OrderRepository.cs
@@ -0,0 +1,21 @@
+﻿using Granny_s_Hot_Box.Models;
+
+namespace Granny_s_Hot_Box.Repositories
+{
+    public class OrderRepository
+    {
+        private readonly IConfiguration _config; 
+        
+        public OrderRepository(IConfiguration config)
+        {
+            _config = config;
+        }
+        public SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection
+            }
+        }
+    }
+}
