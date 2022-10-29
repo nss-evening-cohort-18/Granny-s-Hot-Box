7bc1bf0c707a0cb2a435603618eaa07961089459
Emily Schroeder <estanfill0212@gmail.com>
2022-10-29 11:59:47 -0500

    UserPayments-Interface-Controller-Models-Repository

---
+++ Granny's-Hot-Box/Interfaces/IUserPayments.cs
@@ -0,0 +1,12 @@
+﻿using Granny_s_Hot_Box.Models;
+
+namespace Granny_s_Hot_Box.Interfaces
+{
+    public interface IUserPayment
+    {
+        public List<UserPayments> GetAllUserPayments();
+        public User GetUserPaymentById(int id);
+        public void UpdateUserPayment(UserPayments userPayments);
+        public void CreateUserPayment(UserPayments userPayments);
+    }
+}
---
+++ Granny's-Hot-Box/Repositories/UserPaymentsRepository.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Data.SqlClient;
+using Granny_s_Hot_Box.Models;
+using Granny_s_Hot_Box.Interfaces;
+
+namespace Granny_s_Hot_Box.Repositories
+{
+    public class UserPaymentRepository : BaseRepository
+    {
+        private readonly string _baseSqlSelect = @"SELECT Id,
+                                                          CardName,
+	                                                      AccountNum,
+	                                                      UserId,
+                                                          PaymentTypeId
+                                                          FROM UserPayment";
+
+        public UserPaymentRepository(IConfiguration config) : base(config) { }
+
+        public List<User> GetAllUsers()
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = _baseSqlSelect;
+
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        var results = new List<User>();
+                        while (reader.Read())
+                        {
+                            var user = LoadFromData(reader);
+
+                            results.Add(userPayments);
+                        }
+
+                        return results;
+                    }
+                }
+            }
+        
+
+
