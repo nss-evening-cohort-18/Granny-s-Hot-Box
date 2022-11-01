52906b57cafa677a18de2e26a520bd88a88ad5c1
GiovanniGith <Gbarajas2109@gmail.com>
2022-10-31 19:47:53 -0500

    Created OrderMeals MVC and GetAllOrderMeals method in repo, interface and controller

---
+++ Granny's-Hot-Box/Interfaces/IOrderMeals.cs
@@ -0,0 +1,12 @@
+﻿using Granny_s_Hot_Box.Models;
+
+namespace Granny_s_Hot_Box.Interfaces
+{
+    public interface IOrderMeals
+    {
+        public List <OrderMeals> GetAllOrderMeals();
+       // public OrderMeals GetOrderMealsByOrderId(int id);
+
+      //  public OrderMeals GetOrderMealsByMealProductId(int id);
+    }
+}
---
+++ Granny's-Hot-Box/Models/OrderMeals.cs
@@ -0,0 +1,9 @@
+﻿namespace Granny_s_Hot_Box.Models
+{
+    public class OrderMeals
+    { 
+        public int Id { get; set; }
+        public int orderId { get; set; }
+        public int mealProductId { get; set; }
+    }
+}
