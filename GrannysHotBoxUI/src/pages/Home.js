178f2dde7590c0cfc760e1fc3f66f9989f63f029
Rick Kersmarki <rkersmarki@gmail.com>
2022-11-12 14:15:09 -0600

    homepage and menu page render, menu link works

--- GrannysHotBoxUI/src/pages/Home.js
+++ GrannysHotBoxUI/src/pages/Home.js
@@ -1,10 +1,12 @@
-//DUMMY PAGE DON'T USE!!!
 import React from "react";
+import { Link } from "react-router-dom"
+
 
 export default function Home() {
     return (
         <div>
-            <h1>Home Page</h1>
+            <h1>Granny's Hot Box</h1>
+            <Link to="/menu">See the menu</Link>
         </div>
     )
 }
---
+++ GrannysHotBoxUI/src/pages/Menu.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+    return (
+        <div>
+            <h1>Menu</h1>
+            <h4>render cards for all menu items currently for sale</h4>
+        </div>
+    )
+}
