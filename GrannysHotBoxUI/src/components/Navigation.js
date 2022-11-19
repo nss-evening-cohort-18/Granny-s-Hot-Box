711fc4c0c0adf88cedc11491f7e46e492142fc0a
Giovanni Barajas <gbarajas2109@gmail.com>
2022-11-19 13:02:17 -0600

    Update Navigation.js

--- GrannysHotBoxUI/src/components/Navigation.js
+++ GrannysHotBoxUI/src/components/Navigation.js
@@ -8,30 +8,45 @@ import {
     Button,
     NavbarBrand,
     NavDropdown,
-    Nav
+    NavItem,
+    NavLink
  } from 'react-bootstrap';
  
  
 export const Navigation = ({ user }) => {
-const history = useHistory()
+    const history= useHistory()
     return (
         // potentially switch to NavLink instead of Link
  
         <div>
-            <Navbar  bg="light">
-             <NavbarBrand href="/home">
+            <Navbar bg="light">
+                <NavbarBrand href="/home">
                     <img
                         className="navbar-logo"
                         src={GrannysHotBoxLogo}
                         alt="Granny's Hot Box Logo"
                     />
                 </NavbarBrand>
-               <Nav>
-                    <Nav.Link onClick={() => history.push("/Home")">Home</Nav.Link>
-                    <Nav.Link onClick={() => history.push("/Menu")">Menu</Nav.Link>
-                    <Nav.Link onClick={() => history.push("/Cart")"">Cart</Nav.Link>
-                </Nav>
-                     <div className='user-dropdown'>
+                <div>
+            <Navbar>
+                <NavItem>
+                    <NavLink onClick={() => history.push("/home")}>
+                        Home
+                    </NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink onClick={() => history.push("/test")}>
+                        Test
+                    </NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink onClick={() => history.push("/cart")}>
+                        Cart
+                    </NavLink>
+                </NavItem>
+            </Navbar>
+        </div>
+                <div className='user-dropdown'>
                     <img
                         href="/home"
                         className="profilePic"
@@ -48,9 +63,11 @@ const history = useHistory()
                         </Button>
                     </NavDropdown>
                 </div>
-                </Navbar>
-
-           
+               
+ 
+            </Navbar>
+        </div>
+ 
  
     );
 }
